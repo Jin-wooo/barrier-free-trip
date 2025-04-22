cbb6bb9280dd5704d671b5d187daf89e099ce624
loepi <loepisodeol@gmail.com>
2025-04-22 23:19:57 +0900

    feat: API 라이센스 내용 추가 - 백엔드 API 라이센스 및 아파치 라이센스 문서 추가

--- frontend/app/src/main/java/com/triply/barrierfreetrip/APIFragment.kt
+++ frontend/app/src/main/java/com/triply/barrierfreetrip/APIFragment.kt
@@ -14,22 +14,26 @@ class APIFragment : BaseFragment<FragmentApiBinding>(R.layout.fragment_api) {
         Pair(R.string.api_license_andx_title, R.string.api_license_andx_license),
         Pair(R.string.api_license_kakaomap_title, R.string.api_license_kakaomap_license),
         Pair(R.string.api_license_coroutine_title, R.string.api_license_coroutine_license),
-        Pair(R.string.api_license_gps_title, R.string.api_license_gps_license)
+        Pair(R.string.api_license_gps_title, R.string.api_license_gps_license),
+        Pair(R.string.api_license_jjwt_title, R.string.api_license_jjwt_license),
+        Pair(R.string.api_license_modelmapper_title, R.string.api_license_modelmapper_license),
+        Pair(R.string.api_license_springboot_title, R.string.api_license_springboot_license)
     )
 
     override fun initInViewCreated() {
         val navController = findNavController()
+        val apacheLicenseFullText = requireContext().resources.openRawResource(R.raw.apache_license_2).bufferedReader().use { it.readText() }
 
         with(binding) {
             ivApiBack.setOnClickListener {
                 navController.navigateUp()
             }
 
-            // 리뷰 리사이클러뷰 설정
             rvApi.adapter = APIAdapter().apply {
                 apiList.forEach {
                     addAPIList(Pair(resources.getString(it.first), resources.getString(it.second)))
                 }
+                addAPIList(Pair(resources.getString(R.string.api_license_apache_title), apacheLicenseFullText))
             }
             rvApi.layoutManager = LinearLayoutManager(context).apply {
                 orientation = LinearLayoutManager.VERTICAL
